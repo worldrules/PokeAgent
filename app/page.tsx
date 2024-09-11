543d9303f3318a1d845f28424097c4bf5cae4bfd
worldrulesdev <worldrulesdev@gmail.com>
2024-09-11 00:02:23 -0300

    Ajustes de espaçamento e testes de auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto">
+    <main className="max-w-7xl mx-auto my-12 space-y-5">
       <Dashboard />
     </main>
   );
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import React from 'react'
 
 const Dashboard = () => {
@@ -10,14 +10,17 @@ const Dashboard = () => {
         <>
             {session ? (
                 <>
-                    <h1>Welcome back</h1>
+                    <h1 className='text-3xl text-green-500 font-bold'>
+                        Welcome back, {session.user?.name}
+                    </h1>
+                    <p className='text-2xl font-semibold'>{session.user?.email}</p>
                 </>
             ) : (
-                <>
+                <div className='flex space-x-5 mt'>
                     <h1 className='text-3xl text-red-500 font-bold'>Você não está logado ainda ...</h1>
                     <button onClick={() => signIn("google")} className='border border-black rounded-lg'>Entrar com Google</button>
                     <button onClick={() => signIn("github")} className='border border-black rounded-lg'>Entrar com GitHub</button>
-                </>
+                </div>
             )}
         </>
     )
