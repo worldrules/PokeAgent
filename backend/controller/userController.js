7450901cf0d24544bb3c5f30b4634f62751ab245
worldrulesdev <worldrulesdev@gmail.com>
2024-09-11 05:06:48 -0300

    Controller , rotas e model

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,22 @@
+
+import User from "../model/userModel"
+
+export const create = async (req, res) => {
+    try {
+        const userData = new User(req.body);
+        const { email } = userData;
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ error: "User already exists." })
+        }
+
+        const savedUser = await userData.save();
+        res.status(200).json(savedUser)
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
+
 export const fetch = async (req, res) => {
     try {
         res.json("Hello World")
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { fetch } from '../controller/userController.js'
+import { fetch, create } from '../controller/userController.js'
 
 const route = express.Router();
 
+route.post("/create", create);
 route.get("/fetch", fetch);
 
 export default route;
