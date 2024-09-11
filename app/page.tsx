85ec0b138794c07f0ff10d5bdc1e5d0f7f462603
worldrulesdev <worldrulesdev@gmail.com>
2024-09-11 01:42:53 -0300

    start backend at now

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Dashboard from "@/components/Dashboard";
+import Register from "@/components/Register";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto my-12 space-y-5">
       <Dashboard />
+      <Register />
     </main>
   );
 }
