d84b3011b4d0b14e6918ab6d9fd0c80d406adacc
worldrulesdev <worldrulesdev@gmail.com>
2024-09-12 00:46:40 -0300

    Imagem adicionada ao card

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,21 +1,45 @@
-
-import Link from "next/link"
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { PokemonClient, Pokemon } from 'pokenode-ts';
+import Link from 'next/link';
 
 interface PokemonCardProps {
-    name: string
+    name: string;
 }
 
-
 export function PokemonCard({ name }: PokemonCardProps) {
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            try {
+                const api = new PokemonClient();
+                const data = await api.getPokemonByName(name);
+                setPokemon(data);
+            } catch (error) {
+                console.error('Failed to fetch Pokemon data:', error);
+            }
+        };
+
+        fetchPokemon();
+    }, [name]);
+
+    if (!pokemon) return false;
+
     return (
-        <Link
-            href={"pokemon/" + name}
-            className="group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            key={name + "Card"}
-        >
-            <h2 className={`text-2xl font-semibold`}>
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-            </h2>
+        <Link href={`/pokemon/${pokemon.name}`} passHref>
+            <div className="p-4 border rounded-lg shadow-lg hover:shadow-xl m-3 transition-shadow duration-300 cursor-pointer">
+                <div className="flex flex-col items-center">
+                    <Image
+                        src={pokemon.sprites.front_default || '/default.png'}
+                        alt={pokemon.name}
+                        width={100}
+                        height={100}
+                        className="mb-2"
+                    />
+                    <h2 className="text-lg font-bold capitalize">{pokemon.name}</h2>
+                </div>
+            </div>
         </Link>
-    )
+    );
 }
