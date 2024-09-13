e8bc21a7b63f99175622d275b357cdc89ee23c95
worldrulesdev <worldrulesdev@gmail.com>
2024-09-13 00:26:46 -0300

    MongoDB Connected

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(`Error: ${error.message}`)
-    }
-}
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        });
 
-export default connectToMongoDB
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectToMongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,17 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import route from './routes/userRoute.js'
-import authRoutes from './routes/auth.routes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
 
-app.use(bodyParser.json());
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGO_URL;
 
+connectToMongoDB();
 
-app.use('/api/user', route)
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
 
-
-mongoose.connect(MONGOURL).then(() => {
-    console.log("Database connected");
-    app.listen(PORT, () => {
-        console.log(`Server is runing on port ${PORT}`);
-    })
-}).catch((err) => {
-    console.log(err);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
