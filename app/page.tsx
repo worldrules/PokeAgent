230f8387e1af06ab9b49a0eadb0392d9693a41f8
worldrulesdev <worldrulesdev@gmail.com>
2024-09-13 08:26:04 -0300

    1 part done

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default async function Home() {
     <main className="max-w-7xl mx-auto my-12 space-y-5">
       <PokemonGrid />
       <Dashboard />
-      {/* <Register /> */}
     </main>
   );
 }
