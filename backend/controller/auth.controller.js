fb9ebcc3e5f3fd9a2d79717ae321b6cd0eb23d35
worldrulesdev <worldrulesdev@gmail.com>
2024-09-13 01:01:41 -0300

    novo controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import User from "../model/user.model.js";
+
 export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
+
+        const user = await User.findOne({ username });
     } catch (error) {
 
     }
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-connectToMongoDB();
+dotenv.config();
 
+app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
+    connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
