0cfc927ef2479977e05cfb136291685e719b20a5
worldrulesdev <worldrulesdev@gmail.com>
2024-09-13 14:41:55 -0300

    vercel up

--- lib/authOptions.tsx
+++ lib/authOptions.tsx
@@ -1,27 +1,24 @@
 import { NextAuthOptions } from "next-auth";
-import mongoose from "mongoose";
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
 
 
 
-const connectToDatabase = async () => {
+// const connectToDatabase = async () => {
 
 
 
 
-    if (mongoose.connection.readyState === 1) {
-        return mongoose.connection;
-    }
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    return mongoose.connection;
-};
+//     if (mongoose.connection.readyState === 1) {
+//         return mongoose.connection;
+//     }
+//     await mongoose.connect(process.env.MONGODB_URI as string);
+//     return mongoose.connection;
+// };
 
-const verifyPassword = async (password: string, hashedPassword: string) => {
-    return bcrypt.compare(password, hashedPassword);
-};
+// const verifyPassword = async (password: string, hashedPassword: string) => {
+//     return bcrypt.compare(password, hashedPassword);
+// };
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -33,31 +30,32 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                username: { label: 'Username', type: 'text', placeholder: 'Ash Ketchum 🌝' },
-                password: { label: 'Password', type: 'password', placeholder: '••••••••••••••••••' },
-            },
-            async authorize(credentials) {
-                const client = await connectToDatabase();
-                const user = await User.findOne({ username: credentials?.username }).exec();
+        //     CredentialsProvider({
+        //         name: 'Credentials',
+        //         credentials: {
+        //             username: { label: 'Username', type: 'text', placeholder: 'Ash Ketchum 🌝' },
+        //             password: { label: 'Password', type: 'password', placeholder: '••••••••••••••••••' },
+        //         },
+        //         async authorize(credentials) {
+        //             const client = await connectToDatabase();
+        //             const user = await User.findOne({ username: credentials?.username }).exec();
 
-                if (!user) {
-                    client.close();
-                    throw new Error('No user found!');
-                }
+        //             if (!user) {
+        //                 client.close();
+        //                 throw new Error('No user found!');
+        //             }
 
-                const isValid = await verifyPassword(credentials!.password, user.password);
+        //             const isValid = await verifyPassword(credentials!.password, user.password);
 
-                if (!isValid) {
-                    client.close();
-                    throw new Error('Could not log you in!');
-                }
+        //             if (!isValid) {
+        //                 client.close();
+        //                 throw new Error('Could not log you in!');
+        //             }
 
-                client.close();
-                return { id: user._id.toString(), username: user.username };
-            },
-        }),
-    ],
+        //             client.close();
+        //             return { id: user._id.toString(), username: user.username };
+        //         },
+        //     }),
+        // ],
+    ]
 };
