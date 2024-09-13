228413b6f25a6d0d3cc290aab77dd7232accbe91
worldrulesdev <worldrulesdev@gmail.com>
2024-09-13 02:36:40 -0300

    JWT e controller signup

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import User from "../model/user.model.js";
+import bcrypt from 'bcryptjs';
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
 export const signup = async (req, res) => {
     try {
@@ -10,8 +12,43 @@ export const signup = async (req, res) => {
         }
 
         const user = await User.findOne({ username });
-    } catch (error) {
 
+        if (user) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        //HASH HERE
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+        });
+        if (newUser) {
+            await generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(500).json({ message: 'Failed to create user' });
+        }
+
+    } catch (error) {
+        console.log('Error in signup: ', error.message);
+        res.status(500).json({ message: error.message });
     }
 };
 
